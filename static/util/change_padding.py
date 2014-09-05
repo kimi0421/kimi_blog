6a66158ba8f51330563c3109d1a45ebd9938f332
park_ninja <park_ninja@Kimis-iMac.local>
2014-09-05 09:46:19 -0700

    change the files

--- static/util/change_padding.py
+++ static/util/change_padding.py
@@ -8,7 +8,7 @@ for file_name in all_file_names:
     file_content = open('../ipython_blog/' + file_name, 'r').read()
     file_content = file_content.replace('body {\n  overflow: visible;\n  padding: 8px;\n}\n', 'body {\n  overflow: visible;\n  padding: 0px;\n}\n')
     file_content = file_content.replace('<img src="files/__', '<img src="/static/ipython_blog/files/__')
-    file_content = file_content.replace('src="__', 'src="/static/ipython_blog/files/__')
+    file_content = file_content.replace('src="files/__', 'src="/static/ipython_blog/files/__')
     file_content = file_content.replace('https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS_HTML', 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML')
     f = open('../ipython_blog/' + file_name, 'w')
     f.write(file_content)
